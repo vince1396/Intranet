983548cbd55783eca284b82dfaec236bb3bbbd9b
MarkoStefanovic-98 <markostef@hotmail.fr>
2017-09-21 19:41:15 +0200

    les modis sont admin, professeur,bootstrap, stylecss

--- content/professeur.php
+++ content/professeur.php
@@ -2,126 +2,103 @@
     <div class="container">
         
         <div class="row row1">
-            <div class="col-xs-1"></div>
-            <div class="col-xs-1">
+            <div class="col-xs-12">
                 <span  class="btn btn-info">Classe :</span>
-            </div>
-            <div class="col-xs-2">       
+                     
                 <select class="btn btn-info select_class">
-                    <option value="1">Classe 1</option>
+                    <option value="1"><h1>Classe 1</h1></option>
                     <option value="2">Classe 2</option>
                     <option value="3">Classe 3</option>
                     <option value="4">Classe 4</option>
                     <option value="5">Classe 5</option>
                     <option value="6">Classe 6</option>
                 </select>
+                
+                <span class="btn btn-info">Matière : PROGRAMMATION</span>
+            <div class="col-xs-12">
+               <a href="index.php"><button class="btn btn-primary bouton1"><span class="glyphicon glyphicon-home"></span> Revenir à l'accueil</button></a>
             </div>
-            
-            <div class="col-xs-1">
-                <span  class="btn btn-info">Matière :</span>
-            </div>
-            <div class="col-xs-2">       
-                <select class="btn btn-info select_class">
-                    <option value="1">Algèbre</option>
-                    <option value="2">Anayse</option>
-                    <option value="3">Arithmétique</option>
-                    <option value="4">Géométrie</option>
-                </select>
             </div>
             
 
         </div>
         <br>
         <div class="row">
-            <div class="col-xs-4 onglet_eleve">Elèves</div>
-            <div class="col-xs-4 onglet_eleve">Notes</div>
-            <div class="col-xs-4 onglet_eleve">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Elèves</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Appréciations</h3></div>
         </div>
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Francois</div>
-            <div class="col-xs-4 onglet_eleve2">18 / 19 / 20</div>
-            <div class="col-xs-4 onglet_eleve2">Bien joué !</div>
+              <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Francois</h3></div>
+              <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>18 / 19 / 20</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Bien joué !</h3></div>
         </div>
         
         <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">18 / 16.5 / 18.5</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>18 / 16.5 / 18.5</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Jean</div>
-            <div class="col-xs-4 onglet_eleve2">20 / 14 / 13.5</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Jean</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>20 / 14 / 13.5</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
             <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
             <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
           <div class="row">
-            <div class="col-xs-4 onglet_eleve2">Patric</div>
-            <div class="col-xs-4 onglet_eleve2">Notes</div>
-            <div class="col-xs-4 onglet_eleve2">Appréciations</div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Patric</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3>Notes</h3></div>
+            <div class="col-xs-12 col-md-4 onglet_eleve4 text-center"><h3>Appréciations</h3></div>
         </div>
         
              <div class="row">
-    
-            <div class="col-xs-4 bouton1"></div>
-            <div class="col-xs-4 bouton1"><button class="btn btn-info"><span class="glyphicon glyphicon-plus"></span> Modifier</button></div>
-            <div class="col-xs-4 bouton1"><button class="btn btn-info"><span class="glyphicon glyphicon-plus"></span> Modifier</button></div>
-        </div>
-        
-        <br>
-        
-
-        <br>
-         <div class="row">
-           <div class="col-xs-1"></div>
-            <div class="col-xs-6">
-               <a href="index.php"><button class="btn btn-primary bouton1"><span class="glyphicon glyphicon-home"></span> Revenir à l'accueil</button></a>
-            </div>
-            
-        </div>
-        
-        
+    <div></div>
+            <div class="col-xs-12 col-md-4 bouton1"></div>
+                 <div class="col-xs-12 col-md-4 bouton1"><button class="btn btn-info"><h4>Modifier</h4></button></div>
+                 <div class="col-xs-12 col-md-4 bouton1"><button class="btn btn-info"><h4>Modifier</h4></button></div>
+        </div>    
     </div>
 </div>
