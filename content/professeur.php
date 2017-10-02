f9c0db351645b16ae0588a548d4317ee0ec646c2
FranckChaks <franck1891@hotmail.fr>
2017-10-02 12:18:19 +0200

    Design page prof

--- content/professeur.php
+++ content/professeur.php
@@ -26,19 +26,18 @@
                 ?>
                 <span class="btn btn-info bouton1"> Matière : <?php echo $rep1['nom_m']; ?></span>
 
-            <div class="col-xs-12">
-               <a href="index.php"><button class="btn btn-primary bouton1"><span class="glyphicon glyphicon-home"></span> Revenir à l'accueil</button></a>
-            </div>
+            
             </div>
 
 
         </div>
         <br>
-        <div class="row">
-            <div class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Elèves</h3></div>
-            <div class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Notes</h3></div>
-            <div class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Appréciations</h3></div>
-        </div>
+         <table>
+        <tr class="row">
+            <td class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Elèves</h3></td>
+            <td class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Notes</h3></td>
+            <td class="col-xs-12 col-md-4 onglet_eleve text-center"><h3>Appréciations</h3></td>
+        </tr>
 
         <?php
           if(isset($_POST['submit']))
@@ -47,9 +46,9 @@
             while($rep2 = $req2->fetch())
             {
             ?>
-            <div class="row">
-            <div class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3><?php echo " ".$rep2['prenom']." ".$rep2['nom']; ?></h3></div>
-            <div class="col-xs-12 col-md-4 onglet_eleve1 text-center"><h3>
+            <tr class="row">
+            <td class="col-xs-12 col-md-4 onglet_eleve2 text-center"><h3><?php echo " ".$rep2['prenom']." ".$rep2['nom']; ?></h3></td>
+            <td class="col-xs-12 col-md-4 onglet_eleve1 text-center"><h3>
               <?php
                 $req3 = displayNote($rep2['id_e'], $_POST['classe'], $bdd);
                 while($rep3 = $req3->fetch())
@@ -57,10 +56,13 @@
                     echo " ".$rep3['note']." / ";
                 }
               ?>
-            </h3></div>
-            <div class="col-xs-12 col-md-4 onglet_eleve1 text-center"><h3>Bien joué !</h3></div>
-            </div>
+            </h3></td>
+            <td class="col-xs-12 col-md-4 onglet_eleve1 text-center"><h3>Bien joué !</h3></td>
+            </tr>
             <?php } ?>
+            
+        </table>
+           
             <div class="row">
             <div class="col-xs-12 col-md-4 bouton1"><button class="btn btn-info"><h5><span class="glyphicon glyphicon-pencil"></span> Modifier les élèves</h5></button></div><div class="col-xs-12 col-md-4 bouton1"><button class="btn btn-info"><h5><span class="glyphicon glyphicon-pencil"></span> Modifier les notes</h5></button></div>
           </div> <?php
@@ -95,7 +97,9 @@
     // }
 
 ?>
-
+<div class="col-xs-12">
+               <a href="index.php"><button class="btn btn-primary bouton1"><span class="glyphicon glyphicon-home"></span> Revenir à l'accueil</button></a>
+            </div>
     <div class="row">
 
     <header>
