b8311241ee276cc94616dd2b184256583edadd46
FranckChaks <franck1891@hotmail.fr>
2017-10-05 20:16:45 +0200

    Fonctionnalité Ajout matière espace Gestion des matières

--- content/admin_matiere.php
+++ content/admin_matiere.php
@@ -4,8 +4,6 @@
            <div class="row row1 bouton_titre">
             <div class="col-xs-12">
                <span class="btn btn-primary bouton_titre"><h5>Gestion des matières</h5></span>
-                <span  class="btn btn-info semestre"><h5>Semestre 1 </h5></span>
-                <span  class="btn btn-info semestre"> <h5>Semestre 2</h5></span>
                 <a href="<?=BASE_URL;?>/admin"><button class="btn btn-primary bouton_titre text-center"><span class="glyphicon glyphicon-home"></span> <h5>Revenir à l'accueil </h5></button></a>
             </div> 
            
@@ -14,62 +12,49 @@
         </div>
         <br>
         <table>
+        
+       
         <tr class="row">
-            <td class="col-xs-12 col-md-4 onglet_eleve"><h3>Matière</h3></td>
-            <td class="col-xs-12 col-md-4 onglet_eleve"><h3>Classes</h3></td>
-            <td class="col-xs-12 col-md-4 onglet_eleve"><h3>Enseignants</h3></td>
+            <td class="col-xs-12 col-md-12 onglet_eleve text-center"><h3>Matière</h3></td>
         </tr>
         
-          <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>Français</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
-        <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>Maths</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
-        <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>EDM</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
-            <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>Anglais</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
-        <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>Histoire</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
-        <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>Geographie</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
-        <tr class="row">
-              <td class="col-xs-12 col-md-4 onglet_eleve2"><h3>PPE</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Classe 1, Classe 2</h3></td>
-              <td class="col-xs-12 col-md-4 onglet_eleve1"><h3>Monsieur Machin</h3></td>
-        </tr>
+         <?php
+          $req = displayAllMatiere($bdd);
+            
+          while($rep = $req->fetch())
+          {
+              echo "<tr class='row'>";
+              echo "<td class='col-xs-12 col-md-12 onglet_eleve2'><h3>";
+              echo $rep['nom_m']."</h3></td>";
+          }
+            
+            
+        ?>
         </table>
         
+         <?php
+                
+                if(isset($_POST['submit']))
+                {
+                    $req = AddMatiere($_POST['nom_m'], $bdd);
+                }
+            
+            ?>
         
          <div class="row">
-            <div class="col-xs-12">
-             <div class="bouton1"><button class="btn btn-info"><span class="glyphicon glyphicon-pencil"></span><h5>Modifier les matières</h5></button> <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span><h5>Ajouter une matière</h5></button></div>
-             <div class= "bouton1"><button class="btn btn-info"><span class="glyphicon glyphicon-pencil"></span><h5>Modifier les classes </h5></button> <button class="btn btn-success"><span class="glyphicon glyphicon-plus"></span><h5>Ajouter</h5></button></div>
-             <div class="bouton1"><button class="btn btn-info"><span class="glyphicon glyphicon-pencil"></span><h5>Modifier les enseignants</h5></button></div>
+             <div class="col-xs-12">
+                 <div class="bouton1">
+                   <form method='post'>
+                        <label for="matiere" class="btn btn-primary bouton1"><b>Matière à ajouter: </b>
+                              <input type="text" class="font_color" name="nom_m">
+                          </label>
+                          <button class="btn btn-success" type="submit" name="submit"><span class="glyphicon glyphicon-plus"></span><h5>Ajouter une matière</h5></button>
+                      </form>
+                 </div>
              </div>
-        </div>
-        
-        <br>
-        
-
-        <br>
+         </div>
+              
+           
         
         
     </div>
